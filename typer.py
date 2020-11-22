ad93173daed0fe3e68b8bd09f51f8c88908243c4
bluecover <gurity@163.com>
2020-11-22 18:10:09 +0800

    v1: implement TextLayout and SimpleTokenizer class

--- typer.py
+++ typer.py
@@ -1,95 +1,202 @@
+#!/usr/bin/python3
 # coding: utf-8
+
+import os
+import string
 
 from PIL import Image, ImageFont, ImageDraw
 
-import string
+
+class SimpleTokenizer:
+    """ 简单分词器
+
+        把字符串分割成下面几种单词:
+            - 连续的英文字母
+            - 连续的数字
+            - 其他字符（如中文、空格）单独分词
+            - 标点符号与上一个词结合
+
+        例子:
+            "hello,world.123 你好，世界！"
+            分词结果为：['hello,', 'world.', '123', ' ', '你', '好，', '世', '界！']
+    """
+    def parse(self, text):
+        tokens = []
+        i = 0
+        while i < len(text):
+            if self.is_letter(text[i]):  # 英文
+                j = i + 1
+                while j < len(text) and self.is_letter(text[j]):
+                    j += 1
+                tokens.append(text[i:j])
+                i = j
+            elif text[i].isdigit():  # 数字
+                j = i + 1
+                while j < len(text) and text[j].isdigit():
+                    j += 1
+                tokens.append(text[i:j])
+                i = j
+            elif self.is_punctuation(text[i]):  # 标点
+                if tokens:
+                    tokens[-1] = tokens[-1] + text[i]
+                else:
+                    tokens.append(text[i])
+                i += 1
+            else:  # 其他（中文、空格等）
+                tokens.append(text[i])
+                i += 1
+        return tokens
+
+    @staticmethod
+    def is_letter(s):
+        return 'a' <= s <= 'z' or 'A' <= s <= 'Z'
+
+    @staticmethod
+    def is_punctuation(c):
+        return c in string.punctuation or c in '。，！？‘’“”…—《》（）【】'
 
 
-def is_chinese(c):
-    return u'\u4e00' <= c <= u'\u9fff'
+class TextLayout:
+    """ 实现在矩形(box)内的文字排版功能
 
+    Attributes:
+        text: 绘制的字符串
+        candidate_fonts: 候选字体
+        box_width: 矩形长度
+        box_height: 矩形高度
+        tokenizer: 分词器
+        line_space: 行间距
+        right_padding: 右侧最大空白
+        draw_font: 绘制使用的字体
+        token_lines: 分成行的 token
+    """
 
-def is_punctuation(c):
-    return c in string.punctuation or c in '。，？‘’“”《》'
+    def __init__(self, text, condidate_fonts, box_width, box_height, tokenizer, line_space=0, right_padding=None):
+        self.text = text
+        self.candidate_fonts = condidate_fonts
+        self.box_width = box_width
+        self.box_height = box_height
+        self.tokenizer = tokenizer
+        self.line_space = line_space
+        self.right_padding = right_padding
+        self.draw_font = None
+        self.token_lines = []
 
+    def draw(self, image, x, y, color=255):
+        """ 思路：
+                - 按从大到小的顺序，查找大小合适的的字体
+                - 在查找的过程中，为了测试字体是否合适，会把字符串分词并分行保存在 token_lines 中
+                - 用选中的字体绘制 token_lines
+        """
+        if not self._find_font():
+            raise RuntimeError('Failed to find suitable font')
+        draw = ImageDraw.Draw(image)
+        for tokens in self.token_lines:
+            text = ''.join(tokens)
+            draw.text((x, y), text, font=self.draw_font, fill=color)
+            y += self.draw_font.getsize(text)[1]
+            y += self.line_space
 
-# 分词
-def tokenize(str):
-    return [s for s in str]
+    def _find_font(self):
+        """ 遍历候选字体，返回第一个合适的字体作为绘制使用
+        """
+        # TODO: 使用二分查找
+        if self.draw_font:
+            return True
+        for font in self.candidate_fonts:
+            if self._test_font(font):
+                self.draw_font = font
+                return True
+        return False
 
+    def _test_font(self, font):
+        """测试使用 font 字体能否在 box 范围内绘制 text
+        """
+        # 测试在不分词情况下是否能满足行数要求
+        # 因为 分词后的高度 >= 不分词时的高度，如果不分词时就不满足，则直接返回 False
+        text_width, text_height = font.getsize(self.text)
+        max_line_number = self.box_height // (text_height + self.line_space)  # 最多可容纳多少行
+        min_lines = (text_width + self.box_width) // self.box_width  # 最少需要多少行
+        if min_lines > max_line_number:
+            return False
 
-# 验证使用 font 字体能否在长 max_width, 高 max_height 的矩形内绘制 text
-def validate_font(font, text, tokens, max_width, max_height, line_space=0):
-    # 检查在不分词情况下是否能满足行数要求
-    # 因为 分词后的高度 >= 不分词时的高度，如果不分词时不满足，则直接返回 False
-    text_width, text_height = font.getsize(text)
-    max_line_number = max_height // (text_height + line_space)  # 最多可容纳多少行
-    min_required_lines = (text_width + max_width) // max_width  # 最少需要多少行
-    if min_required_lines > max_line_number:
-        return False, None
+        # 测试分词分行
+        if not self._break_tokens_to_lines(font):
+            return False
+        if len(self.token_lines) > max_line_number:
+            return False
 
-    lines = make_token_lines(font, tokens, max_width)
-    if len(lines) > max_line_number:
-        return False, None
+        return True
 
-    return True, lines
+    def _break_tokens_to_lines(self, font):
+        """ 把字符串分成 token，再分组到多个 line 中
+        """
+        tokens = self.tokenizer.parse(self.text)
 
+        token_lines = []
+        cur_line = []
+        cur_width = 0
+        for token in tokens:
+            token_width = font.getsize(token)[0]
+            if cur_width + token_width <= self.box_width:
+                cur_width += token_width
+                cur_line.append(token)
+            else:
+                if self.right_padding:  # 检查右侧 padding
+                    padding = self.box_width - cur_width
+                    if padding > self.right_padding:
+                        return False
+                token_lines.append(cur_line)
+                cur_line = [token]
+                cur_width = token_width
+        if cur_line:
+            token_lines.append(cur_line)
 
-# 遍历不同 font size 的字体，选择最大的可用字体
-def find_fitting_font(font_sizes, text, tokens, max_width, max_height, line_space=0):
-    for size in reversed(font_sizes):
-        font = ImageFont.truetype('./FONT.TTF', size)
-        fit, lines = validate_font(font, text, tokens, max_width, max_height, line_space)
-        if fit:
-            return font, lines
-
-    return None, None
-
-
-# 根据行宽度，把 token 归集成 line
-def make_token_lines(font, tokens, line_width):
-    lines = []
-    cur_line = []
-    cur_width = 0
-    for token in tokens:
-        token_width = font.getsize(token)[0]
-        if cur_width + token_width <= line_width:
-            cur_width += token_width
-            cur_line.append(token)
-        else:
-            lines.append(cur_line)
-            cur_line = [token]
-            cur_width = token_width
-    if cur_line:
-        lines.append(cur_line)
-
-    return lines
-
-
-def draw_lines(x, y, image, font, token_lines, line_height, color=255):
-    draw = ImageDraw.Draw(image)
-    for tokens in token_lines:
-        text = ''.join(tokens)
-        draw.text((x, y), text, font=font, fill=color)
-        y += line_height
+        self.token_lines = token_lines
+        return True
 
 
 if __name__ == '__main__':
-    text = '谋杀我的完美完美完美完美完美完美偶像偶像谋杀我的完美完美完美完美完美完美偶像偶像'
-    image = Image.open("./1.png")
-    width, height = int(image.size[0] * 0.8), int(image.size[1] * 0.2)
-    x, y = int(image.size[0] * 0.1), int(image.size[1] * 0.6)
+    # 创建候选字体
+    # 最大字体 300, 以 3 为间隔，从大到小创建候选字体
+    max_font_size = 300
+    condidate_fonts = [
+        ImageFont.truetype('./FONT.TTF', sz)
+        for sz in range(max_font_size, 0, -3)
+    ]
 
-    tokens = tokenize(text)
+    cover_image_files = [
+        './1.png',
+    ]
 
-    # 以矩形的 height 为上限，大小间隔为 5 的序列作为候选 font size
-    min_font_size, max_font_size = 5, height
-    candidate_font_sizes = range(min_font_size, max_font_size, 5)
-    font, lines = find_fitting_font(candidate_font_sizes, text, tokens, width, height, 0)
-    if not font:
-        print('fitting font is not found')
-        exit()
+    for fn, title, author in [
+            ('long', '谋杀我的完美完美完美完美完美完美偶像偶像', '作者很长长长长长长长长长长长长'),
+            ('medium', '浮城旧梦', '小米贝贝'),
+            ('short', '沉默', 'Yohi'),
 
-    line_height = font.getsize(text)[1]
-    draw_lines(x, y, image, font, lines, line_height)
-    image.save('./result/sample-out.png')
+            # 选做项1: 避免违反排版规则的标点符号出现在行首
+            ('punctuation', '试管医，手记！八细胞：承诺（完整版）', '小米贝贝'),
+
+            # 选做项2: 纯英文、中英文数字混排
+            ('en', 'Fantastic Beasts: The Crimes of Grindelwald', 'J.K. Rowling'),
+            ('mix', 'The Lost Stars: 云都', '风倾'),
+            ('digit', '逃离20岁', '小米贝贝')]:
+
+        for index, file in enumerate(cover_image_files):
+            image = Image.open(file)
+            width = int(image.size[0] * 0.8)
+            height = int(image.size[1] * 0.2)
+            left = int(image.size[0] * 0.1)
+            top = int(image.size[1] * 0.6)
+            text_layout = TextLayout(
+                title, condidate_fonts, width, height,
+                tokenizer=SimpleTokenizer(),
+                line_space=10,
+                right_padding=5,
+            )
+            text_layout.draw(image, left, top)
+
+            outdir = './{}'.format(index + 1)
+            if not os.path.exists(outdir):
+                os.makedirs(outdir)
+            image.save('{}/{}.png'.format(outdir, fn))
--- typer2.py
+++ typer2.py
@@ -3,12 +3,56 @@
 
 from PIL import Image, ImageFont, ImageDraw
 
+import string
 
-class Tokenizer:
-    """ 分词器
+
+class SimpleTokenizer:
+    """ 简单分词器
+
+        把字符串分割成下面几种单词:
+          - 连续的英文字母
+          - 连续的数字
+          - 其他字符（如中文、空格）单独分词
+          - 标点符号与上一个词结合
+
+        例子：
+          "hello,world.123 你好，世界！"
+          分词结果为：['hello,', 'world.', '123', ' ', '你', '好，', '世', '界！']
     """
     def parse(self, text):
-        return [s for s in text]
+        tokens = []
+        i = 0
+        while i < len(text):
+            if self.is_letter(text[i]):  # 英文
+                j = i + 1
+                while j < len(text) and self.is_letter(text[j]):
+                    j += 1
+                tokens.append(text[i:j])
+                i = j
+            elif text[i].isdigit():  # 数字
+                j = i + 1
+                while j < len(text) and text[j].isdigit():
+                    j += 1
+                tokens.append(text[i:j])
+                i = j
+            elif self.is_punctuation(text[i]):  # 标点
+                if tokens:
+                    tokens[-1] = tokens[-1] + text[i]
+                else:
+                    tokens.append(text[i])
+                i += 1
+            else:  # 其他（中文、空格等）
+                tokens.append(text[i])
+                i += 1
+        return tokens
+
+    @staticmethod
+    def is_letter(s):
+        return 'a' <= s <= 'z' or 'A' <= s <= 'Z'
+
+    @staticmethod
+    def is_punctuation(c):
+        return c in string.punctuation or c in '。，！？‘’“”…—《》（）【】'
 
 
 class TextLayout:
@@ -16,60 +60,62 @@ class TextLayout:
 
     Attributes:
         text: 绘制的字符串
-        font_size_range: 字体大小范围
+        candidate_fonts: 候选字体
         box_width: 矩形长度
         box_height: 矩形高度
         line_space: 行间距
         right_padding: 右侧最大空白
         tokenizer: 分词器
-        font: 绘制使用的字体
+        draw_font: 绘制使用的字体
         token_lines: 分成行的 token
     """
 
-    def __init__(self, text, font_file, font_size_range, box_width, box_height, line_space, right_padding, tokenizer):
+    def __init__(self, text, condidate_fonts, box_width, box_height, line_space, right_padding, tokenizer):
         self.text = text
-        self.font_file = font_file
-        self.font_size_range = font_size_range
+        self.candidate_fonts = condidate_fonts
         self.box_width = box_width
         self.box_height = box_height
         self.line_space = line_space
         self.right_padding = right_padding
         self.tokenizer = tokenizer
-        self.font = None
+        self.draw_font = None
         self.token_lines = []
 
-    # 寻找合适的字体
     def _find_font(self):
-        if self.font:
+        """ 遍历候选字体，返回第一个合适的字体作为绘制使用
+        """
+        if self.draw_font:
             return True
-        for size in reversed(self.font_size_range):
-            font = ImageFont.truetype(self.font_file, size)
+        for font in self.candidate_fonts:
             if self._test_font(font):
-                self.font = font
+                self.draw_font = font
                 return True
         return False
 
-    # 测试使用 font 字体能否在 box 范围内绘制 text
     def _test_font(self, font):
-        # 检查在不分词情况下是否能满足行数要求
-        # 因为 分词后的高度 >= 不分词时的高度，如果不分词时不满足，则直接返回 False
+        """测试使用 font 字体能否在 box 范围内绘制 text
+        """
+        # 测试在不分词情况下是否能满足行数要求
+        # 因为 分词后的高度 >= 不分词时的高度，如果不分词时就不满足，则直接返回 False
         text_width, text_height = font.getsize(self.text)
         max_line_number = self.box_height // (text_height + self.line_space)  # 最多可容纳多少行
         min_lines = (text_width + self.box_width) // self.box_width  # 最少需要多少行
         if min_lines > max_line_number:
             return False
 
+        # 测试分词分行
         if not self._break_tokens_to_lines(font):
             return False
-
         if len(self.token_lines) > max_line_number:
             return False
 
         return True
 
-    # 把 token 分成多个 line
     def _break_tokens_to_lines(self, font):
+        """ 把字符串分成 token，再分组到多个 line 中
+        """
         tokens = self.tokenizer.parse(self.text)
+
         token_lines = []
         cur_line = []
         cur_width = 0
@@ -79,9 +125,10 @@ class TextLayout:
                 cur_width += token_width
                 cur_line.append(token)
             else:
-                padding = self.box_width - cur_width
-                if padding > self.right_padding:
-                    return False
+                if self.right_padding:  # 检查右侧 padding
+                    padding = self.box_width - cur_width
+                    if padding > self.right_padding:
+                        return False
                 token_lines.append(cur_line)
                 cur_line = [token]
                 cur_width = token_width
@@ -97,31 +144,34 @@ class TextLayout:
         draw = ImageDraw.Draw(image)
         for tokens in self.token_lines:
             text = ''.join(tokens)
-            draw.text((x, y), text, font=self.font, fill=color)
-            y += self.font.getsize(text)[1]
+            draw.text((x, y), text, font=self.draw_font, fill=color)
+            y += self.draw_font.getsize(text)[1]
             y += self.line_space
 
 
 if __name__ == '__main__':
-    text = '谋杀我的完美偶像一二三四五六七八九十'
+    text = '哈哈哈哈哈哈哈hello,world.123 你好，世界！'
+    text = '试管医，手记！八细胞：承诺（完整版）'
     image = Image.open('./1.png')
     box_width = int(image.size[0] * 0.8)
     box_height = int(image.size[1] * 0.2)
     left = int(image.size[0] * 0.1)
     top = int(image.size[1] * 0.6)
 
-    # 以 box_height 为字体大小上限, 3 为间隔
-    max_font_size = box_height
-    min_font_size = 5
-    font_size_range = range(min_font_size, max_font_size, 3)
+    # 以 box_height 为上限, 3 为间隔，从大到小创建候选 font
+    max_font_size = 300
+    condidate_fonts = [
+        ImageFont.truetype('./FONT.TTF', sz)
+        for sz in range(max_font_size, 0, -3)
+    ]
 
     tl = TextLayout(
         text,
-        './FONT.ttf', font_size_range,
+        condidate_fonts,
         box_width, box_height,
-        line_space=10,
+        line_space=5,
         right_padding=10,
-        tokenizer=Tokenizer()
+        tokenizer=SimpleTokenizer()
     )
     tl.draw(left, top, image)
     image.save('./result/sample-out.png')
